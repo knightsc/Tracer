5cb96b0580a248452c9228830b685a72efeec1dd
Scott Knight <4534275+knightsc@users.noreply.github.com>
2019-07-04 11:26:07 -0400

    Initial Commit

---
+++ Tracer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Tracer
+//
+//  Created by Scott Knight on 7/4/19.
+//  Copyright © 2019 Scott Knight. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Tracer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Tracer
+//
+//  Created by Scott Knight on 7/4/19.
+//  Copyright © 2019 Scott Knight. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
